880b5596fd76e2c42122dbc3affeb87210439966
elisa <elisa@DESKTOP-NOHK2QH>
2022-10-15 15:20:41 -0700

    Add project files.

---
+++ Activity 10 CST-150/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Activity_10_CST_150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* ERROR DOCUMENTATION: 
+            This program's error documentation can be found in set.cs, the set class.*/
+
+            //Create new sets from the Set class. 
+            Set A = new Set();
+            Set B = new Set();
+            //Put elements in the set. 
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+            //Display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+            //Display original sets 
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+        }
+    }
+}
---
+++ Activity 10 CST-150/Set.cs
@@ -0,0 +1,102 @@
+﻿using Baseline.ImTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Activity_10_CST_150
+{
+    internal class Set
+    {
+        private List<int> elements;
+        public Set() { elements = new List<int>(); }
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else {
+                elements.Add(val);
+                return true;
+            }
+        }
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (val == elements[i]) return true;
+
+                // [ERROR] else return false; 
+                /* [REASON] The else case has been removed because it is an invalid case in this context.
+                 *  An else statement is not used to do condition checking and should not be used in the
+                 *  same manner as if or if else. Else is intended to execute alternative code in circumstances
+                 *  where if and else if conditions return false. Attempting to use it in this context
+                 * will produce unwanted results. */
+
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            { str += i + " "; }
+            return str; }
+        public void clearSet()
+        { elements.Clear(); }
+
+            /* [LOGIC ERROR] 
+           COMMENTED OUT METHOD: 
+
+            * public Set union(Set rhs )        
+            * {                       
+            * for(int i = 0; i < rhs.elements.Count; i++)            
+            * {                
+            * this.addElement( rhs.elements[ i ] );            
+            * }                       
+            * return rhs;        
+            * }    
+            * }
+            * }
+            
+            [REASON]
+            In order to produce the desired results, we must be able to compare our union
+            set to our existing elements in the program. The previous method 
+            would not produce the desired results. */
+
+        public Set union(Set rhs)
+
+        {
+
+            // Create a new set. Set C will be our union set. 
+            Set c = new Set();
+
+            // Iterate
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (!c.containsElement(elements[i]))
+                { // If 'c' does not contain the existing list element, then add it to c.
+                    c.addElement(elements[i]);
+                }
+            }
+
+            // Iterate through the passed object's elements list
+            for (int i = 0; i < rhs.elements.Count; i++)
+            {
+                if (!c.containsElement(rhs.elements[i]))
+                { // If the list 'c' does not contain the current element of the passed object's list add the current element to the set. 
+                    c.addElement(rhs.elements[i]);
+                }
+            }
+
+            return c; // Return the set. 
+
+        }
+
+    }
+
+}
+
+
+
